7d4b44f95584d1984f33d4e652875b04b74dbbbf
zyu dong <15835310877@163.com>
2019-11-12 16:40:01 +0800

    movie.sql

---
+++ movie.sql
@@ -0,0 +1,63 @@
+--电影
+create table Movie(
+	mID int primary key,
+	mname nvarchar(20),
+	movieLanguage nvarchar(10),
+	duration int,		--时长
+	area nvarchar(20),
+	releaseTime datetime,
+	content text,
+	price float
+)
+
+--职员
+create table Worker(
+	wid int primary key,
+	wcountry nvarchar(20),
+	wname nvarchar,
+)
+
+--导演关系
+create table direct(
+	wid int,
+	mID int,
+	constraint d_prim primary key(wid, mID)
+)
+
+--出演关系
+create table act(
+	wid int,
+	mID int,
+	constraint a_prim primary key(wid, mID)
+)
+
+--编剧关系
+create table author(
+	wid int,
+	mID int,
+	constraint at_prim primary key(wid, mID)
+)
+
+--类型 type是关键字，故改为typ
+create table Typ(
+	typename nvarchar(30) primary key
+)
+
+--电影类型关系
+create table mtype(
+	mID int,
+	typename nvarchar(30),
+	constraint mt_prim primary key(mID, typename)
+)
+
+--标签
+create table tag(
+	tag nvarchar(30) primary key
+)
+
+--电影标签关系
+create table mtag(
+	mID int,
+	tag nvarchar(30),
+	constraint mtg_prim primary key(mID, tag)
+)
