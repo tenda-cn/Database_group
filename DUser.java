1795dcd570adee7eb330408742fcaaf5877f1b89
wts-smile <wsdsfcd@126.com>
2019-11-12 15:55:58 +0800

    用户java文件

---
+++ DUser.java
@@ -0,0 +1,70 @@
+package sql;
+
+public class DUser {
+    private Stringtring username;
+    private int ID;
+    private String pwd;
+    private String email;
+    private String gender;
+    private String Bir;
+    private String Tel;
+
+    public Stringtring getUsername() {
+        return username;
+    }
+
+    public void setUsername(Stringtring username) {
+        this.username = username;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getBir() {
+        return Bir;
+    }
+
+    public void setBir(String bir) {
+        Bir = bir;
+    }
+
+    public String getTel() {
+        return Tel;
+    }
+
+    public void setTel(String tel) {
+        Tel = tel;
+    }
+
+    public DUser() {
+    }
+}
