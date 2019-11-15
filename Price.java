388500120952c472f76ee034f33a14c990495503
Lidan-Amber <Lidan991122@163.com>
2019-11-15 12:02:08 +0800

    Creat Price.java

---
+++ Price.java
@@ -0,0 +1,25 @@
+package entity;
+
+public class Price {
+	int PId;
+	String festival;
+	String item;
+	public int getPId() {
+		return PId;
+	}
+	public void setPId(int pId) {
+		PId = pId;
+	}
+	public String getFestival() {
+		return festival;
+	}
+	public void setFestival(String festival) {
+		this.festival = festival;
+	}
+	public String getItem() {
+		return item;
+	}
+	public void setItem(String item) {
+		this.item = item;
+	}
+}
