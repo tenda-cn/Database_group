778ed7ed644c30cd53563e660c6eaebb82eb36b9
fucongchong <45562950+fucongchong@users.noreply.github.com>
2019-11-15 09:14:45 +0800

    new comment test class
    Add mID test.

---
+++ CommentDAOImplTest.java
@@ -0,0 +1,92 @@
+package test; 
+
+import dao.DAOFactory;
+
+
+
+
+import dao.commentDAO;
+import daoImpl.commentDAOImpl;
+import entity.comment;
+
+
+import java.util.ArrayList;
+
+public class CommentDAOImplTest {
+
+    commentDAO commDAO=DAOFactory.getCommentDAO();
+
+    public void testInsertComment() throws Exception {
+    //TODO: Test goes here...
+        boolean b=commDAO.insertComment(new comment(102,"2019-11-12","非常好的一部电影","手机",200,9.3,"看过"));
+        
+    }
+
+    /**
+    *
+    * Method: updateComment(comment comm)
+    *
+    */
+    public void testUpdateComment() throws Exception {
+    //TODO: Test goes here...
+        boolean b=commDAO.updateComment(new comment(102,"2019-10-2","据说男主演得很棒","电脑",301,8.9,"没看过"));
+     
+    }
+
+    /**
+    *
+    * Method: deleteComment(int cid)
+    *
+    */
+    public void testDeleteComment() throws Exception {
+        boolean b=commDAO.deleteComment(102);
+        
+    //TODO: Test goes here...
+    }
+
+    /**
+    *
+    * Method: getComment(int cid)
+    *
+    */
+    public void testGetComment() throws Exception {
+    //TODO: Test goes here...
+        comment comm=commDAO.getComment(102);
+        System.out.println("cid:"+comm.getCid());
+        System.out.println("c_time:"+comm.getC_time());
+        System.out.println("c_content:"+comm.getC_content());
+        System.out.println("c_facility:"+comm.getC_facility());
+        System.out.println("useful_amount:"+comm.getUseful());
+        System.out.println("c_score:"+comm.getC_score());
+        System.out.println("see_movie_status:"+comm.getSee_movie_status());
+    }
+
+    /**
+    *
+    * Method: getComment()
+    *
+    */
+    public void testGetComment2() throws Exception {
+    //TODO: Test goes here...
+        ArrayList<comment> list=commDAO.getComment();
+        System.out.println(list.size());
+        for(comment comm:list){
+            System.out.println(comm.getC_time());
+        }
+    }
+    
+    
+    public void testGetCommentByMID() throws Exception{
+    	int mID1=189981;
+    	commentDAOImpl cdi=new commentDAOImpl();
+    	ArrayList<comment> list=null;
+        list=cdi.getCommentByMID(mID1);
+    	for(comment comm:list)
+    	{
+    		System.out.println(comm.getC_time());//测试mID为189981电影对应的所有评价的评价时间
+    	}
+    }
+    		
+    
+
+} 
